ca1ebaedf933db223ee0369ca7f25608a1270c0b
X-Byte Dev <61307582+xbytedev@users.noreply.github.com>
2026-01-16 19:55:55 +0530

    Update worker.py

--- worker.py
+++ worker.py
@@ -142,7 +142,7 @@ def _setup_chrome_options():
     # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    #options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
